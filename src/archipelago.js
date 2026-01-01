200c79f78255be57c2363f73ad48ff4e511e0f4e
MarioManTAW <mariomantaw@gmail.com>
2026-01-01 10:53:51 -0600

    Fix IDs for Fill and Free-Form Select tools

--- src/archipelago.js
+++ src/archipelago.js
@@ -128,7 +128,7 @@ function update() {
 			case "Polygon":
 			case "Ellipse":
 			case "Rounded Rectangle":
-				$('#TOOL_' + item.split("/")[0].replace(" ", "_").toUpperCase() + '').removeClass("disabled");
+				$('#TOOL_' + item.split("/")[0].split(" With")[0].replace(" ", "_").replace("-", "_").toUpperCase() + '').removeClass("disabled");
 				break;
 			case "Progressive Canvas Width":
 				if (w < 800) w += slotData.canvas_size_increment ?? 100;
